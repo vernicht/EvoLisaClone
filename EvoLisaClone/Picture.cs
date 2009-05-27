aa22555062ee7910924c957ad2211b09210ff448
vernicht <jadergrupos@yahoo.com.br>
2009-05-26 21:47:32 -0300

    Added file structure

---
+++ EvoLisaClone/Picture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvoLisaClone
+{
+    class Picture
+    {
+        public Picture()
+        {
+        }
+
+        public Picture(IEnumerable<Picture> parents, Random random)
+        {
+        }
+    }
+}
